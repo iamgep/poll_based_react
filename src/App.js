0e872c349dbbe0a29e2ec75a3c22c768083ecbbd
Jepoi <iamgep@proton.me>
2023-04-26 17:30:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+export default App;
+
+const API_BASE_URL = '<YOUR_API_BASE_URL>';
 
 function App() {
+  const [polls, setPolls] = useState([]);
+  const [question, setQuestion] = useState('');
+  const [options, setOptions] = useState('');
+  const [selectedPoll, setSelectedPoll] = useState(null);
+  const [votedOption, setVotedOption] = useState(null);
+
+  useEffect(() => {
+    getPolls();
+  }, []);
+
+  async function getPolls() {
+    const response = await axios.get(`${API_BASE_URL}/polls`);
+    setPolls(response.data);
+  }
+
+  async function createPoll(e) {
+    e.preventDefault();
+    await axios.post(`${API_BASE_URL}/polls`, { question, options: options.split(',') });
+    setQuestion('');
+    setOptions('');
+    getPolls();
+  }
+
+  async function vote(e) {
+    e.preventDefault();
+    await axios.put(`${API_BASE_URL}/polls/${selectedPoll.id}/vote`, { selectedOption: votedOption });
+    getPolls();
+    setSelectedPoll(null);
+    setVotedOption(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Poll App</h1>
+
+      <h2>Create a new poll</h2>
+      <form onSubmit={createPoll}>
+        <label>
+          Question:
+          <input type="text" value={question} onChange={(e) => setQuestion(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Options (separated by commas):
+          <input type="text" value={options} onChange={(e) => setOptions(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Create Poll</button>
+      </form>
+
+      <h2>All Polls</h2>
+      <ul className="polls">
+        {polls.map((poll) => (
+          <li key={poll.id} onClick={() => setSelectedPoll(poll)}>
+            {poll.question}
+          </li>
+        ))}
+      </ul>
+
+      {selectedPoll && (
+        <div>
+          <h2>{selectedPoll.question}</h2>
+          <form onSubmit={vote}>
+            {selectedPoll.options.map((option, index) => (
+              <label key={index}>
+                <input
+                  type="radio"
+                  name="vote"
+                  value={index}
+                  checked={votedOption === index}
+                  onChange={(e) => setVotedOption(Number(e.target.value))}
+                />
+                {option}
+              </label>
+            ))}
+            <br />
+            <button type="submit">Vote</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+        
+
